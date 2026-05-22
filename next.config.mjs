edf2568bf2b44e364ee85bedc9968b844bc4265d
Alessandro Torre <torre.alessandro70@gmail.com>
2026-05-22 19:21:06 +0200

    Disattivo ESLint durante build Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   allowedDevOrigins: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
