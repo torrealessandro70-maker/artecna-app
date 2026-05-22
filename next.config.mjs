fad4bc19ad759ffc3b4811ef2c3996475bb1741a
Alessandro Torre <torre.alessandro70@gmail.com>
2026-05-22 18:24:23 +0200

    Correggo configurazione Next per Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    root: process.cwd(),
-  },
-};
+  allowedDevOrigins: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+  ],
+}
 
-export default nextConfig;
+export default nextConfig
