46b99c82414865e5a3c6de3452596b002225e6f5
Alessandro Torre <torre.alessandro70@gmail.com>
2026-06-14 16:02:17 +0200

    Sposta titolo PresenzeCostiOperaiPanel - build verde

--- app/components/PresenzeCostiOperaiPanel.tsx
+++ app/components/PresenzeCostiOperaiPanel.tsx
@@ -1,11 +1,20 @@
 'use client'
 
+import type { CSSProperties, ReactNode } from 'react'
+
 type Props = {
-  children: React.ReactNode
+  cardStyle: CSSProperties
+  children: ReactNode
 }
 
 export default function PresenzeCostiOperaiPanel({
+  cardStyle,
   children,
 }: Props) {
-  return <>{children}</>
+  return (
+    <div style={cardStyle}>
+      <h2>Presenze / costi operai</h2>
+      {children}
+    </div>
+  )
 }
