349267ffd365fe4e263a10ea38a20b4718fdda37
Alessandro Torre <torre.alessandro70@gmail.com>
2026-06-15 18:30:28 +0200

    Estrae OperaiPresenzeContainer - build verde

--- app/page.tsx
+++ app/page.tsx
@@ -99,6 +99,8 @@ import AccontiEconomiaPanel from './components/AccontiEconomiaPanel'
 import RiepilogoCostiEconomiaPanel from './components/RiepilogoCostiEconomiaPanel'
 import CantieriEconomiaPanel from './components/CantieriEconomiaPanel'
 import HomeDashboardPanel from './components/HomeDashboardPanel'
+import OperaiPresenzeContainer from './components/OperaiPresenzeContainer'
+
 import type {
   Cantiere,
   Rapportino,
@@ -11382,108 +11384,64 @@ WebkitOverflowScrolling: 'touch',
     buttonPrimary={buttonPrimary}
     buttonSecondary={buttonSecondary}
   />
-)}{/* ================= OPERAI - PRESENZE / COSTI ================= */}
-{(
+)}{(
   pagineAperte.includes('operai-presenze') ||
   (!modalitaMulti &&
     sezioneAttiva === 'operai' &&
     sottoSezioneOperai === 'presenze')
 ) && (
- <PresenzeCostiOperaiPanel
-  cardStyle={cardStyle}
->
-    
-
-{timbraturaInModifica && (
-  <PopupModificaTimbratura
-    titolo="Modifica presenza"
-    dataTimbraturaModifica={dataTimbraturaModifica}
-    setDataTimbraturaModifica={setDataTimbraturaModifica}
-    oraEntrataModifica={oraEntrataModifica}
-    setOraEntrataModifica={setOraEntrataModifica}
-    oraUscitaModifica={oraUscitaModifica}
-    setOraUscitaModifica={setOraUscitaModifica}
-    parseOra={parseOra}
-    onChiudi={() => setTimbraturaInModifica(null)}
-    onSalva={salvaModificaTimbratura}
-    buttonSecondary={buttonSecondary}
-    buttonPrimary={buttonPrimary}
-  />
-)}
-<PresenzaManualePanel
-  dataPresenzaManuale={dataPresenzaManuale}
-  setDataPresenzaManuale={setDataPresenzaManuale}
-  operaioPresenzaManuale={operaioPresenzaManuale}
-  setOperaioPresenzaManuale={setOperaioPresenzaManuale}
-  cantierePresenzaManuale={cantierePresenzaManuale}
-  setCantierePresenzaManuale={setCantierePresenzaManuale}
-  oraEntrataManuale={oraEntrataManuale}
-  setOraEntrataManuale={setOraEntrataManuale}
-  oraUscitaManuale={oraUscitaManuale}
-  setOraUscitaManuale={setOraUscitaManuale}
-  operaiAnagrafica={operaiAnagrafica}
-  cantieri={cantieri}
-  aggiungiPresenzaManuale={aggiungiPresenzaManuale}
-  buttonPrimary={buttonPrimary}
+ <OperaiPresenzeContainer
+  {...{
+    cardStyle,
+    timbraturaInModifica,
+    dataTimbraturaModifica,
+    setDataTimbraturaModifica,
+    oraEntrataModifica,
+    setOraEntrataModifica,
+    oraUscitaModifica,
+    setOraUscitaModifica,
+    parseOra,
+    setTimbraturaInModifica,
+    salvaModificaTimbratura,
+    buttonSecondary,
+    buttonPrimary,
+    dataPresenzaManuale,
+    setDataPresenzaManuale,
+    operaioPresenzaManuale,
+    setOperaioPresenzaManuale,
+    cantierePresenzaManuale,
+    setCantierePresenzaManuale,
+    oraEntrataManuale,
+    setOraEntrataManuale,
+    oraUscitaManuale,
+    setOraUscitaManuale,
+    operaiAnagrafica,
+    cantieri,
+    aggiungiPresenzaManuale,
+    dataDa,
+    setDataDa,
+    dataA,
+    setDataA,
+    cantiereGrafico,
+    setCantiereGrafico,
+    inputStyle,
+    timbrature,
+    totaleCostoPeriodo,
+    totaleOrePeriodo,
+    calcolaOre,
+    calcolaCostoTimbratura,
+    formatMoney,
+    badgeStyle,
+    preparaModificaOperaio,
+    cambiaStatoOperaio,
+    eliminaOperaio,
+    costoPerCantiereOggi,
+    timbratureOggi,
+    erroreTimbratura,
+    setStatoTimbraturaModifica,
+    eliminaTimbratura,
+  }}
 />
-
-
-
-<FiltroPresenzeCostiPanel
-  dataDa={dataDa}
-  setDataDa={setDataDa}
-  dataA={dataA}
-  setDataA={setDataA}
-  cantiereGrafico={cantiereGrafico}
-  setCantiereGrafico={setCantiereGrafico}
-  cantieri={cantieri}
-  inputStyle={inputStyle}
-/>
-
-
-
-   <PresenzePeriodoSummaryPanel
-  timbrature={timbrature}
-  operaiAnagrafica={operaiAnagrafica}
-  dataDa={dataDa}
-  dataA={dataA}
-  cantiereGrafico={cantiereGrafico}
-  totaleCostoPeriodo={totaleCostoPeriodo}
-  totaleOrePeriodo={totaleOrePeriodo}
-  calcolaOre={calcolaOre}
-  calcolaCostoTimbratura={calcolaCostoTimbratura}
-  formatMoney={formatMoney}
-  badgeStyle={badgeStyle}
-  preparaModificaOperaio={preparaModificaOperaio}
-  cambiaStatoOperaio={cambiaStatoOperaio}
-  eliminaOperaio={eliminaOperaio}
-  buttonPrimary={buttonPrimary}
-  buttonSecondary={buttonSecondary}
-/>
-
-
-
-
-
-  <CostoPerCantiereOggiPanel
-  costoPerCantiereOggi={costoPerCantiereOggi}
-  formatMoney={formatMoney}
-/>
-
-   <DettaglioPresenzeOggiPanel
-  timbratureOggi={timbratureOggi}
-  formatMoney={formatMoney}
-  calcolaCostoTimbratura={calcolaCostoTimbratura}
-  erroreTimbratura={erroreTimbratura}
-  setTimbraturaInModifica={setTimbraturaInModifica}
-  setDataTimbraturaModifica={setDataTimbraturaModifica}
-  setOraEntrataModifica={setOraEntrataModifica}
-  setOraUscitaModifica={setOraUscitaModifica}
-  setStatoTimbraturaModifica={setStatoTimbraturaModifica}
-  eliminaTimbratura={eliminaTimbratura}
-  buttonSecondary={buttonSecondary}
-/>
-</PresenzeCostiOperaiPanel>
 )}
 
 
