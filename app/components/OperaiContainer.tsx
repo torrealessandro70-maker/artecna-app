112cd3f2695388e42132785ef9ed165c4ef19e8d
Alessandro Torre <torre.alessandro70@gmail.com>
2026-06-16 23:15:01 +0200

    Estende OperaiContainer con timbrature - build verde

--- app/components/OperaiContainer.tsx
+++ app/components/OperaiContainer.tsx
@@ -13,51 +13,42 @@ export default function OperaiContainer(props: Props) {
     <>
       {/* ================= OPERAI - ANAGRAFICA ================= */}
       {(
-        p.pagineAperte.includes('operai-anagrafica') ||
-        (!p.modalitaMulti &&
-          p.sezioneAttiva === 'operai' &&
-          p.sottoSezioneOperai === 'anagrafica')
-      ) && (
-        <OperaiAnagraficaPanel
-          cardStyle={p.cardStyle}
-          operaioInModifica={p.operaioInModifica}
-          annullaModificaOperaio={p.annullaModificaOperaio}
-          salvaModificaOperaio={p.salvaModificaOperaio}
-          nomeOperaioModifica={p.nomeOperaioModifica}
-          setNomeOperaioModifica={p.setNomeOperaioModifica}
-          telefonoOperaioModifica={p.telefonoOperaioModifica}
-          setTelefonoOperaioModifica={p.setTelefonoOperaioModifica}
-          qualificaOperaioModifica={p.qualificaOperaioModifica}
-          setQualificaOperaioModifica={p.setQualificaOperaioModifica}
-          pinOperaioModifica={p.pinOperaioModifica}
-          setPinOperaioModifica={p.setPinOperaioModifica}
-          costoOrarioOperaioModifica={p.costoOrarioOperaioModifica}
-          setCostoOrarioOperaioModifica={p.setCostoOrarioOperaioModifica}
-          statoOperaioModifica={p.statoOperaioModifica}
-          setStatoOperaioModifica={p.setStatoOperaioModifica}
-          notaOperaioModifica={p.notaOperaioModifica}
-          setNotaOperaioModifica={p.setNotaOperaioModifica}
-          nomeOperaio={p.nomeOperaio}
-          setNomeOperaio={p.setNomeOperaio}
-          telefonoOperaio={p.telefonoOperaio}
-          setTelefonoOperaio={p.setTelefonoOperaio}
-          qualificaOperaio={p.qualificaOperaio}
-          setQualificaOperaio={p.setQualificaOperaio}
-          pinOperaio={p.pinOperaio}
-          setPinOperaio={p.setPinOperaio}
-          costoOrarioOperaio={p.costoOrarioOperaio}
-          setCostoOrarioOperaio={p.setCostoOrarioOperaio}
-          aggiungiOperaio={p.aggiungiOperaio}
-          operaiFiltrati={p.operaiFiltrati}
-          badgeStyle={p.badgeStyle}
-          formatMoney={p.formatMoney}
-          preparaModificaOperaio={p.preparaModificaOperaio}
-          cambiaStatoOperaio={p.cambiaStatoOperaio}
-          eliminaOperaio={p.eliminaOperaio}
-          buttonPrimary={p.buttonPrimary}
-          buttonSecondary={p.buttonSecondary}
-        />
-      )}
+  p.pagineAperte.includes('operai-timbrature') ||
+  (!p.modalitaMulti &&
+    p.sezioneAttiva === 'operai' &&
+    p.sottoSezioneOperai === 'timbrature')
+) && (
+  <OperaiTimbraturePanel
+    cardStyle={p.cardStyle}
+    timbraturaInModifica={p.timbraturaInModifica}
+    dataTimbraturaModifica={p.dataTimbraturaModifica}
+    setDataTimbraturaModifica={p.setDataTimbraturaModifica}
+    oraEntrataModifica={p.oraEntrataModifica}
+    setOraEntrataModifica={p.setOraEntrataModifica}
+    oraUscitaModifica={p.oraUscitaModifica}
+    setOraUscitaModifica={p.setOraUscitaModifica}
+    setTimbraturaInModifica={p.setTimbraturaInModifica}
+    setStatoTimbraturaModifica={p.setStatoTimbraturaModifica}
+    parseOra={p.parseOra}
+    salvaModificaTimbratura={p.salvaModificaTimbratura}
+    operaioTimbratura={p.operaioTimbratura}
+    setOperaioTimbratura={p.setOperaioTimbratura}
+    cantiereTimbratura={p.cantiereTimbratura}
+    setCantiereTimbratura={p.setCantiereTimbratura}
+    pinTimbratura={p.pinTimbratura}
+    setPinTimbratura={p.setPinTimbratura}
+    operaiAttivi={p.operaiAttivi}
+    cantieri={p.cantieri}
+    timbratureOggi={p.timbratureOggi}
+    timbraEntrataConPin={p.timbraEntrataConPin}
+    timbraUscitaConPin={p.timbraUscitaConPin}
+    eliminaTimbratura={p.eliminaTimbratura}
+    calcolaCostoTimbratura={p.calcolaCostoTimbratura}
+    formatMoney={p.formatMoney}
+    buttonPrimary={p.buttonPrimary}
+    buttonSecondary={p.buttonSecondary}
+  />
+)}
     </>
   )
 }
