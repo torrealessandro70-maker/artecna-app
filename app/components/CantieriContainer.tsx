9e1b68ed976bb7dfaff2afe54fa22e3bfce2b488
Alessandro Torre <torre.alessandro70@gmail.com>
2026-06-17 19:23:50 +0200

    Esplicita props CantieriSchedaPanel - build verde

--- app/components/CantieriContainer.tsx
+++ app/components/CantieriContainer.tsx
@@ -4,12 +4,46 @@ import CantieriSchedaPanel from './CantieriSchedaPanel'
 import CantieriAnalisiDocumentoPanel from './CantieriAnalisiDocumentoPanel'
 import CantieriEconomiaPanel from './CantieriEconomiaPanel'
 
-type CantieriContainerProps = any
+interface CantieriContainerProps {
+  [key: string]: any
+}
 
 export default function CantieriContainer(props: CantieriContainerProps) {
+const {
+  cardStyle,
+  cantieri,
+  cantiereScheda,
+  setCantiereScheda,
+  ricercaCantiereEconomia,
+  setRicercaCantiereEconomia,
+  mostraConclusiEconomia,
+  setMostraConclusiEconomia,
+  calcoloEconomiaCantiere,
+  formatMoney,
+  inputStyle,
+  buttonPrimary,
+  buttonSecondary,
+  ...rest
+} = props
   return (
     <>
-      <CantieriSchedaPanel {...props} />
+      <CantieriSchedaPanel
+  cardStyle={cardStyle}
+  cantieri={cantieri}
+  cantiereScheda={cantiereScheda}
+  setCantiereScheda={setCantiereScheda}
+  ricercaCantiereEconomia={ricercaCantiereEconomia}
+  setRicercaCantiereEconomia={setRicercaCantiereEconomia}
+  mostraConclusiEconomia={mostraConclusiEconomia}
+  setMostraConclusiEconomia={setMostraConclusiEconomia}
+  calcoloEconomiaCantiere={calcoloEconomiaCantiere}
+  formatMoney={formatMoney}
+  inputStyle={inputStyle}
+  buttonPrimary={buttonPrimary}
+  buttonSecondary={buttonSecondary}
+
+  {...rest}
+/>
       <CantieriAnalisiDocumentoPanel {...props} />
       <CantieriEconomiaPanel {...props} />
     </>
